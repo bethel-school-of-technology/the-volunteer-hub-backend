56c0d4bf1b4d0e8f5d505b03f787cab28e89e5dd
Donovan Bradley <dbjpk12@gmail.com>
2019-12-11 22:22:02 -0700

    Update user model fields to unique.

--- models/Users.js
+++ models/Users.js
@@ -3,15 +3,18 @@ const mongoose = require('mongoose');
 const UserSchema = mongoose.Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     email: {
         type: String,
         required: true,
+        unique: true
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     }
 });
 
