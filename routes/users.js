4ecd169765240870a51e7950472fba2486921ad8
hdreyer <hannahdreyer@gmail.com>
2019-12-15 10:19:30 -0600

    Updated CORS code

--- routes/users.js
+++ routes/users.js
@@ -28,27 +28,9 @@ router.post('/signUp', (req, res, next) => {
 //Route for a user to  login.
 router.post('/login', function (req, res, next) {
   User.findOne({
-     'username': req.body.username
-  })
-  .then(user => {
-    if (!user) {
-      console.log("User not found.");
-      res.status(401).json({ message: "Login failed."});
-    } else {
-      let passwordMatch = authService.comparePasswords(req.body.password, user.password);
-      if (passwordMatch) {
-        let token = authService.signUser(user);
-        res.cookie('jwt', token);
-        console.log(user);
-        console.log(token);
       } else {
         console.log("Wrong password!");
       }
-      
-      return res.status(200).send();
-    }
-  })
 });
 
 
-module.exports = router;
